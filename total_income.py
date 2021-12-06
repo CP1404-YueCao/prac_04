0cc6b1f8f9e04c62916d47dfd0ce378ecd55c2b3
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-06 23:13:40 +0800

    Use enumerate method

--- total_income.py
+++ total_income.py
@@ -16,10 +16,9 @@ def display_report(incomes):
     """Display the report of incomes."""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month+1, income, total))
 
 
 main()
