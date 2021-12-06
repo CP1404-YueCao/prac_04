9a1fdc9a8d7a082f90a51ceea876c37613520883
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-07 00:29:54 +0800

    Add an error checking program for user's input

--- quick_picks.py
+++ quick_picks.py
@@ -8,6 +8,9 @@ MAXIMUM = 45
 def main():
     """Ask user how many lines of numbers they want to generate and display the numbers"""
     number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Please enter a valid number!")
+        number_of_quick_picks = int(input("How many quick picks? "))
 
     for i in range(number_of_quick_picks):
         quick_picks = []
