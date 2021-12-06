4fd3c893b1567fbf464d63c4f04432cfd3506c22
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-06 23:58:50 +0800

    Add a list of usernames

--- list.exercises.py
+++ list.exercises.py
@@ -8,3 +8,7 @@ print("The last number is", numbers[-1])
 print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the number is", sum(numbers) / len(numbers))
+
+# 2. Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
