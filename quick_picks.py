592760627474839814d1c3eebb862448c6819c9d
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-07 00:26:56 +0800

    Add a program to ask the user how many lines of numbers they want to generate

--- quick_picks.py
+++ quick_picks.py
@@ -3,3 +3,14 @@ import random
 NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
+
+
+def main():
+    """Ask user how many lines of numbers they want to generate and display the numbers"""
+    number_of_quick_picks = int(input("How many quick picks? "))
+
+
+main()
+
+
+
