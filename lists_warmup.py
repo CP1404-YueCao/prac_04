182ced8967d7628536c9ef63aa94ca4d0abc2c53
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-06 22:13:01 +0800

    Add 4 Python expressions to achieve the comments above

--- lists_warmup.py
+++ lists_warmup.py
@@ -8,3 +8,14 @@ print(5 in numbers)
 print(7 in numbers)
 print("3" in numbers)
 print(numbers + [6, 5, 3])
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+print(numbers)
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)
+# Get all the elements from numbers except the first two
+print(numbers[2:])
+# Check if 9 is an element of numbers
+print(9 in numbers)
